d01a5afc58943ed28411e835ed4e39e321c8469d
AliAbdallah454 <alikhaledabdallah454@gmail.com>
2024-11-20 11:05:20 +0200

    before changing next and prev

--- src/main.rs
+++ src/main.rs
@@ -5,31 +5,18 @@ use my_consistent_hashing::consistent_hashing::ConsistentHashing;
 
 fn main() {
 
-    let mut cons = ConsistentHashing::<DefaultHasher>::new(2);
+    let mut cons = ConsistentHashing::<DefaultHasher>::new(100+1);
 
-    // let mut nodes = vec![];
-
-    // println!("Inserting");
     let begin = Instant::now();
-    for i in 0..6 {
-
-        if let Ok(item) = cons.add_node(&format!("node{}", i)) {
+    for i in 0..10_000 {
+        cons.add_node(&format!("node{}", i));
+        // if let Ok(item) = cons.add_node(&format!("node{}", i)) {
             // for tran in item {
             //     println!("Trans : {:?}", tran);
             // }
-        }
-        cons.get_current_state();
+        // }
+        // cons.get_current_state();
     }
-    // println!("Done in {:?}", begin.elapsed());
-
-    // let begin = Instant::now();
-    // let x = cons.remove_node("node4").unwrap();
-    // for t in x {
-    //     println!("Trans : {:?}", t);
-    // }
-    // cons.get_current_state();
-    // println!("Done in {:?}", begin.elapsed());
-    println!("Changing v_node_count");
-    cons.set_virtual_nodes_count(3);
+    println!("Done in {:?}", begin.elapsed());
 
 }
